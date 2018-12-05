879c75f682726c852083a4c34e8e53db3917f7d2
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-05 12:45:33 +0000

    Reuse a.sh

--- 2018/5/b.sh
+++ 2018/5/b.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
 input=$(cat -)
-for a in {a..z}; do
-    tr -d "$a${a^^}"<<<"$input" |\
-    sed -Ef <(echo "::";for a in {a..z}; do A=${a^^};echo "s/($a$A|$A$a)//g;t:"; done) |\
-    tr -d '\n' | wc -m
-done | sort -n | head -1
+for a in {a..z}; do tr -d "$a${a^^}"<<<"$input" | ./a.sh done | sort -n | head -1
