11a772f82938f1804433adc182d0dda7a21c4784
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-05 12:46:33 +0000

    Missing ';'

--- 2018/5/b.sh
+++ 2018/5/b.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 input=$(cat -)
-for a in {a..z}; do tr -d "$a${a^^}"<<<"$input" | ./a.sh done | sort -n | head -1
+for a in {a..z}; do tr -d "$a${a^^}"<<<"$input" | ./a.sh; done | sort -n | head -1
