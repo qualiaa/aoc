414cc0a9c2cb89439b37e7a1d43fc98aeb91265a
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-06 20:41:51 +0000

    Better .hs and .py solutions for day four

--- 2018/5/ab.py
+++ 2018/5/ab.py
@@ -2,34 +2,14 @@
 
 import sys
 
-polymer = list(map(ord,sys.stdin.read().split()[0]))
-
-combine = lambda a,b: abs(a-b) == 32
-
-def singleStep(p):
-    ix = iter(range(0, len(p)-1))
-    result = []
-    try: 
-        for i in ix:
-            if combine(p[i], p[i+1]):
-                next(ix)
-            else:
-                result.append(p[i])
-        result.append(p[-1])
-    except: pass
-    return result
-
-def completeReaction(p):
-    q = singleStep(p)
-    while(q != p):
-        p = q
-        q = singleStep(p)
+def readPoly(s):
+    p = []
+    for u in s:
+        try: p.pop() if p[-1] in [u-32,u+32] else p.append(u)
+        except: p.append(u)
     return p
 
-print(len(completeReaction(polymer)))
-
-print(min([len(completeReaction(list(filter(lambda x: x not in unit, polymer))))
-        for unit in zip(range(65,97),range(97,123))]))
-
-
-
+polymer = list(map(ord,sys.stdin.read().split()[0]))
+print(len(readPoly(polymer)))
+print(min([len(readPoly(filter(lambda c: c not in unit,polymer)))
+            for unit in zip(range(65,97),range(97,123))]))
