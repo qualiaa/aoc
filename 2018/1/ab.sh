893fe4e34da6a45c938a45bf71c2fc50cb3737c2
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-07 02:25:11 +0000

    Replace #with #!/bin/sh

--- 2018/1/ab.sh
+++ 2018/1/ab.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
+#!/bin/sh
 
 input=$(cat -)
-{tr -d '\n' <<<"$input";echo} | bc
+{ tr -d '\n' <<<"$input"; echo; } | bc
 yes -- "$input" | awk '{ fs[f] = 1; f += $0; if (f in fs) { print f; exit } }'
