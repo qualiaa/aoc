6a96cc08fdc5357e92a4a7fc9eaf3640cbb81960
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-08 23:56:06 +0000

    Completely redo day seven using the State monad

--- 2018/7/ab.hs
+++ 2018/7/ab.hs
@@ -1,91 +1,63 @@
+import Control.Monad (ap, liftM, join)
+import Control.Monad.State.Strict (evalState)
 import Data.Char (isUpper, ord)
-import Data.List (minimumBy, sortBy, (\\))
-import Data.Maybe (isJust, maybeToList)
---import qualified Data.Map as M
-import Data.Ord (comparing)
+import Data.List (foldl', sort)
+import Data.Maybe (catMaybes)
+import qualified Data.Map as M
+import TaskState
 
--- do not write haskell when tired
--- state monad pls
+performTasksAlone :: TaskState [Task]
+performTasksAlone = do
+    todo <- completableTasks
+    case todo of
+        (t:_) -> finishTask t >> performTasksAlone
+        []    -> reverse <$> finishedTasks
 
--- Minutes Job
-type Task    = Char
-type DepList = [(Task, Task)]
-data Worker  = Worker Int (Maybe Task) deriving (Show)
+assignTasks :: TaskState ()
+assignTasks = do
+    todo <- readyTasks
+    time <- currentTime
+    let assignWorker ([],ws) w          = ([], w:ws)
+        assignWorker (ts,ws) w@(Just _) = (ts, w:ws)
+        assignWorker (t:ts,ws) Nothing  = 
+            let w = Just (t, (time + duration t)) in (ts, w:ws)
 
-worker = Worker 0 Nothing
-initWorkers = replicate 5 worker
+    liftM (snd . foldl' assignWorker (todo,[])) workers >>= setWorkers
+          
 
-duration task = (ord task) - (ord 'A') + 61
-
-giveTask (Worker m Nothing) t = Worker m' $ Just t
-    where m' = duration t + m
-
-nextTasks list solved = filter (`notElem` (map snd list)) (['A'..'Z'] \\ solved)
-nextTask l = head . nextTasks l
-initTasks depList = nextTasks depList ""
-
-solveTask :: DepList -> Task -> DepList
-solveTask deps task = if task `notElem` (initTasks deps) then error "Uhoh"
-    else filter (\(dep,_) -> dep /= task) deps
-
-solveTasks :: DepList -> [Task] -> DepList
-solveTasks = foldl solveTask 
-
-depenendencyOrder :: DepList -> [Task]
-depenendencyOrder l = reverse $ depenendencyOrder' l ""
-
-    where depenendencyOrder' [] solved = (nextTasks [] solved) ++ solved
-          depenendencyOrder' depList solved = depenendencyOrder' newList' solved'
-            where newList' = solveTask depList task
-                  task     = nextTask depList solved
-                  solved'  = task:solved
-
-assignedTasks :: [Worker] -> [Task]
-assignedTasks = foldl (\ts (Worker _ t) -> maybeToList t ++ ts) []
-
-assignWorkers :: Int -> [Worker] -> [Task] -> [Worker]
-assignWorkers time workers tasks = snd $ foldl assignWorker (tasks',[]) workers
-
-    where tasks' = tasks \\ (assignedTasks workers)
-          assignWorker :: ([Task],[Worker]) -> Worker -> ([Task], [Worker])
-          assignWorker ([],ws) w = ([],w:ws)
-          assignWorker ((t:ts),ws) w@(Worker m Nothing) = (ts, (Worker (duration t + time) (Just t)):ws)
-          assignWorker (ts,ws) w = (ts,w:ws)
-
-nextSolvedTasks :: [Worker] -> (Int, [Task],[Worker])
-nextSolvedTasks workers = (solvedTime, solvedTasks, nextWorkers)
-
-    where (solvedTasks, nextWorkers) = foldl completeWorker ([], []) workers
-          assignedWorkers = filter (\(Worker _ t) -> isJust t) workers
-          solvedTime = minimum $ map (\(Worker m _) -> m) assignedWorkers
-          completeWorker :: ([Task], [Worker]) -> Worker -> ([Task], [Worker])
-          completeWorker (ts, ws) w@(Worker _ Nothing) = (ts, w:ws)
-          completeWorker (ts, ws) w@(Worker m (Just t)) = 
-                case m `compare` solvedTime of
-                    LT -> error "Wuh wo"
-                    EQ -> (t:ts, (Worker m Nothing):ws)
-                    GT -> (ts, w:ws)
-
-workerSolve :: DepList -> [Worker] -> [Worker]
-workerSolve l ws = workerSolve' l ws "" 0
-
-    where workerSolve' [] workers solved time = assignWorkers time workers (nextTasks [] solved)
-          workerSolve' depList workers solved time =
-                let assignedWorkers = assignWorkers time workers $ nextTasks depList solved
-                    (time', solved', workers') = nextSolvedTasks assignedWorkers
-                    depList' = solveTasks depList solved'
-                in workerSolve' depList' workers' (solved' ++ solved) time'
-        
-
-{-
-timeToSolve :: DepList -> Int
-timeToSolve = maximum . map (\(Worker t _) -> t) . workerSolve
--}
-
-main = do
-    depList <- sortBy (comparing snd) .map (list2toTuple . filter isUpper . drop 1) . lines <$> getContents
-    putStrLn $ depenendencyOrder depList
-    print $ workerSolve depList initWorkers
+finishWorkers :: TaskState ()
+finishWorkers = do
+    time <- currentTime
+    let finishWorker :: Worker -> TaskState Worker
+        finishWorker Nothing = return Nothing
+        finishWorker w@(Just (task, t)) =
+            if time /= t then return w else finishTask task >> return Nothing
+    setWorkers =<< (join $ sequence . map finishWorker <$> workers)
+                
+finishAssignedTask :: TaskState ()
+finishAssignedTask = do
+    minimum . map snd . catMaybes <$> workers >>= setTime
+    finishWorkers
     
 
-list2toTuple [a,b] = (a,b)
+minDuration = 60
+duration :: Task -> Time
+duration t = ord t - ord 'A' + 1 + minDuration
+
+performTasksWithWorkers :: TaskState Time
+performTasksWithWorkers = do
+    finished <- liftM (==) (sort <$> finishedTasks) `ap` allTasks
+    if finished
+        then currentTime
+        else assignTasks >> finishAssignedTask >> performTasksWithWorkers
+
+main = do
+    depList <- M.fromListWith (++) .
+        map (fmap (:[]) . list2toPair . filter isUpper . drop 1) .  lines <$> getContents
+    let initialState = initState depList 5
+
+    putStrLn $ evalState performTasksAlone initialState
+    print $ evalState performTasksWithWorkers initialState
+
+
+list2toPair [a,b] = (a,b)
