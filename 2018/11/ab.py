8facea771c7a4bc40fe77f4bcee1b848bb8a2ab1
Jamie Bayne <jamiebayne@hotmail.com>
2018-12-11 13:01:31 +0000

    Use convolve instead of convolve2d for speed

--- 2018/11/ab.py
+++ 2018/11/ab.py
@@ -2,7 +2,7 @@
 
 import sys
 import numpy as np
-from scipy.signal import convolve2d
+from scipy.signal import convolve
 
 s = int(sys.stdin.read())
 
@@ -11,19 +11,13 @@ r = ix[1] + 10
 grid = ((r*ix[0] + s)*r // 10 // 10 % 10) - 5
 
 def get_conv(k):
-    sums = convolve2d(grid, np.ones((k,k)), "valid")
+    sums = convolve(grid, np.ones((k,k)), "valid")
     i = np.argmax(sums)
     yx = np.array(np.unravel_index(i, sums.shape))
     return (sums.ravel()[i], tuple(yx[::-1]+1) + (k,))
 
 print(get_conv(3)[1][:2])
 
-maxV = -sys.maxsize
-maxCoords = None
-for i in range(1,301):
-    v, coords = get_conv(i)
-    if v > maxV:
-        maxV = v
-        maxCoords = coords
-
-print(maxCoords)
+maxsums = [get_conv(i+1) for i in range(300)]
+maxVal, maxCoord = maxsums[max(range(300), key=lambda i: maxsums[i][0])]
+print(maxCoord)
